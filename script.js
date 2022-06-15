27cd115fd393414c119c6d05e1694d60081b6e0d
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-06-15 10:49:25 -0300

    contador de cartas ok

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 let totaldecartas;
+totaldecartas = prompt("Com quantas cartas gostaria de jogar? OBS: apenas números pares (4 ~ 14)");
+if (totaldecartas % 2 !== 0 || totaldecartas < 4 || totaldecartas > 14) {
+    correction ();
+} else {
+    distribuction ();
+}
 
-totaldecartas = prompt("Com quantas cartas gostaria de jogar? OBS: apenas números pares (4 ~ 14)")
-while (totaldecartas % 2 !== 0 || totaldecartas < 4 || totaldecartas > 14) {
-    alert("Eu disse APENAS números pares, entre 4 ou 14 cartas, escreve direito ai")
-    totaldecartas = prompt("Com quantas cartas gostaria de jogar? OBS: apenas números pares (4 ~ 14)")
+function correction () {
+    alert("Eu disse APENAS números pares, entre 4 ou 14 cartas, escreve direito ai");
+    totaldecartas = prompt("Com quantas cartas gostaria de jogar? OBS: apenas números pares (4 ~ 14)");
+    if (totaldecartas % 2 !== 0 || totaldecartas < 4 || totaldecartas > 14) {
+        correction ();
+    } else {
+        distribuction ();
     }
+}
+
+function distribuction () {
+    const elemento = document.querySelector(".cards")
+    let contador;
+    for (contador = 0; contador < totaldecartas; contador++) {
+        elemento.innerHTML = elemento.innerHTML + (`<div class="card"><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/front.png"></div>`)
+    }
+}
