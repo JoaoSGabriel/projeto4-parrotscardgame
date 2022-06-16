b72be979e7ac4fc2ffa9debdd393f87e2baacb9d
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-06-16 12:16:14 -0300

    emabaralhador de cartas ok

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let totaldecartas;
+let vetorA = [];
+let vetorB = [];
+let vx;
 totaldecartas = prompt("Com quantas cartas gostaria de jogar? OBS: apenas números pares (4 ~ 14)");
 if (totaldecartas % 2 !== 0 || totaldecartas < 4 || totaldecartas > 14) {
     correction ();
 } else {
-    distribuction ();
+    cardrandom ();
 }
 
 function correction () {
@@ -12,8 +15,48 @@ function correction () {
     if (totaldecartas % 2 !== 0 || totaldecartas < 4 || totaldecartas > 14) {
         correction ();
     } else {
-        distribuction ();
+        cardrandom ();
     }
+}
+
+function rotacionar(elemento) {
+    let a = elemento.querySelector(".front-face");
+    a.classList.toggle("rodafrente");
+    let b = elemento.querySelector(".back-face");
+    b.classList.toggle("rodatras");
+    b.classList.toogle("back-face");
+}
+
+function cardrandom () {
+    const imagegifs = [
+        "./img/bobrossparrot.gif",
+        "./img/bobrossparrot.gif",
+        "./img/explodyparrot.gif",
+        "./img/explodyparrot.gif",
+        "./img/fiestaparrot.gif",
+        "./img/fiestaparrot.gif",
+        "./img/metalparrot.gif",
+        "./img/metalparrot.gif",
+        "./img/revertitparrot.gif",
+        "./img/revertitparrot.gif",
+        "./img/tripletsparrot.gif",
+        "./img/tripletsparrot.gif",
+        "./img/unicornparrot.gif",
+        "./img/unicornparrot.gif",
+    ]
+    
+    let copiador = 0;
+    while (copiador < totaldecartas) {
+        vetorA.push(imagegifs[copiador]);
+        copiador++;
+    }
+    
+    for (j = 0; j < totaldecartas; j++) {
+        index = parseInt(Math.random()* vetorA.length);
+        vx = vetorA.splice(index,1);
+        vetorB.push(vx[0]);
+    }
+    distribuction ();
 }
 
 function distribuction () {
@@ -26,16 +69,8 @@ function distribuction () {
           <img src="img/front.png" />
         </div>
         <div class="back-face face">
-          <img src="img/fiestaparrot.gif" />
+          <img src="${vetorB[contador]}" />
         </div>
     </div>`)
     }
 }
-
-function rotacionar(elemento) {
-    let a = elemento.querySelector(".front-face");
-    a.classList.toggle("rodafrente");
-    let b = elemento.querySelector(".back-face");
-    b.classList.toggle("rodatras");
-    b.classList.toogle("back-face");
-  }
