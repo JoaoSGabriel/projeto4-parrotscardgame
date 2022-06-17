c2994f357d081393eeb6d0db0b3a8599d4ecde31
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-06-17 11:12:38 -0300

    embaralhador PERFEITO

--- script.js
+++ script.js
@@ -2,17 +2,13 @@ let totaldecartas;
 let vetorA = [];
 let vetorB = [];
 let vx;
-totaldecartas = prompt("Com quantas cartas gostaria de jogar? OBS: apenas números pares (4 ~ 14)");
-if (totaldecartas % 2 !== 0 || totaldecartas < 4 || totaldecartas > 14) {
-    correction ();
-} else {
-    cardrandom ();
-}
+let qtdclicks = 0;
 
+correction ();
 function correction () {
-    alert("Eu disse APENAS números pares, entre 4 ou 14 cartas, escreve direito ai");
     totaldecartas = prompt("Com quantas cartas gostaria de jogar? OBS: apenas números pares (4 ~ 14)");
     if (totaldecartas % 2 !== 0 || totaldecartas < 4 || totaldecartas > 14) {
+        alert("Eu disse APENAS números pares, entre 4 ou 14 cartas, escreve direito ai");
         correction ();
     } else {
         cardrandom ();
@@ -20,34 +16,34 @@ function correction () {
 }
 
 function rotacionar(elemento) {
+    contadordeclicks ();
     let a = elemento.querySelector(".front-face");
     a.classList.toggle("rodafrente");
     let b = elemento.querySelector(".back-face");
     b.classList.toggle("rodatras");
-    b.classList.toogle("back-face");
+}
+
+function contadordeclicks () {
+    qtdclicks++;
 }
 
 function cardrandom () {
     const imagegifs = [
         "./img/bobrossparrot.gif",
-        "./img/bobrossparrot.gif",
-        "./img/explodyparrot.gif",
         "./img/explodyparrot.gif",
         "./img/fiestaparrot.gif",
-        "./img/fiestaparrot.gif",
-        "./img/metalparrot.gif",
         "./img/metalparrot.gif",
         "./img/revertitparrot.gif",
-        "./img/revertitparrot.gif",
         "./img/tripletsparrot.gif",
-        "./img/tripletsparrot.gif",
-        "./img/unicornparrot.gif",
         "./img/unicornparrot.gif",
     ]
     
     let copiador = 0;
-    while (copiador < totaldecartas) {
-        vetorA.push(imagegifs[copiador]);
+    while (copiador < totaldecartas/2) {
+        index = parseInt(Math.random()* imagegifs.length);
+        vx = imagegifs.splice(index,1);
+        vetorA.push(vx[0]);
+        vetorA.push(vx[0]);
         copiador++;
     }
     
@@ -69,7 +65,7 @@ function distribuction () {
           <img src="img/front.png" />
         </div>
         <div class="back-face face">
-          <img src="${vetorB[contador]}" />
+          <img src='${vetorB[contador]}'/>
         </div>
     </div>`)
     }
